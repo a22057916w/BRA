020e13b3d10e58921b5bb22369b79f0647a30cde
naitea9527 <a22057916w@gmail.com>
2024-11-21 00:12:50 +0800

    新增任務分配流程及控制選項

--- worker/worker3.py
+++ worker/worker3.py
@@ -38,13 +38,15 @@ class Worker(BaseWorker):
         start_second=None,
         batch_size=1,
         enable_track=None,
-        io_backend="FFMPEG"
+        io_backend="FFMPEG",
+        enable_task_scheduling=False
     ):
         super().__init__(EOI=['LOSS_CAPTURE', 'GET_CAPTURE'])
         configStream = open(worker_cfg, 'r')
         self.config = yaml.safe_load(configStream)
         configStream.close()
         self.vcfg_path = view_cfg
+        
 
         if 'enable' not in self.config['track_opt'] and enable_track is None:
             self.config['track_opt']['enable'] = True
@@ -109,19 +111,25 @@ class Worker(BaseWorker):
         self.distance_metrics = None
         self.wash_correct_metrics = None
 
-        # 新增時間分割相關的屬性
-        self.current_task = 0  # 0: 口罩, 1: 社交距離, 2: 洗手
-        self.task_timer = 0    # 計數當前任務執行的幀數
-        self.last_mask_stats = None      # 儲存上一次的口罩統計
-        self.last_distance_stats = None  # 儲存上一次的距離統計
-        self.last_wash_stats = None      # 儲存上一次的洗手統計
+        # 時間分割相關的屬性
+        self.enable_task_scheduling = enable_task_scheduling
+        self.current_task = None
+        self.task_timer = None
+
+        # 如果進行任務分配，初始計時器
+        logger.debug(f"self.enable_task_scheduling: {self.enable_task_scheduling}")
+        if self.enable_task_scheduling:
+            self.current_task = 0  # 0: 洗手, 1: 口罩, 2: 社交距離
+            self.task_timer = 0    # 計數當前任務執行的幀數
+
 
     def lazy_init(self):
         if self.config['track_opt']['enable'] and self.reid:
             self.byteTracker = BYTETracker_reid(type('',(object,),self.track_parameter)(), frame_rate=self.fps)
         else:
-            self.byteTracker = BYTETracker(type('',(object,),self.track_parameter)(), frame_rate=self.fps)
+            self.byteTracker = BYTETracker(type('',(object,),self.track_parameter)(), frame_rate=self.fps)       
         self.washFilter = EventFilter(self.vcfg_path, self.config['general']['record_life'], self.fps)
+        
         self.person_count_metrics = cmb.ACBlock(
             self.fps*self.config['general']['metrics_duration'],
             self.fps*self.config['general']['metrics_update_time'],
@@ -138,6 +146,13 @@ class Worker(BaseWorker):
             self.fps*self.config['general']['metrics_duration'],
             self.fps*self.config['general']['metrics_update_time'],
         )
+        # For both task-scheduling and non-scheduling
+        self.overall_metrics = {
+            "person_count": None,
+            "mask": None,
+            "distance": None,
+            "wash_correct": None
+        }
 
     def _examWork(self):
         return True
@@ -151,18 +166,12 @@ class Worker(BaseWorker):
             'fids':FIDs,
         }
 
-        # 更新任務計時器
-        self.task_timer += length
-        frames_per_switch = self.fps * 30
+        # 如果進行任務分配，更新任務計時器
+        if self.enable_task_scheduling:
+            self.task_timer += length
+            frames_per_task = self.fps * self.config['general']['metrics_update_time']  # 設定任務禎數
 
         
-        # 如果達到一分鐘,切換到下一個任務
-        if self.task_timer >= frames_per_switch:
-            self.current_task = (self.current_task + 1) % 3
-            self.task_timer = 0
-            logger.info(f"Switching to task: {['Mask Detection', 'Social Distance', 'Hand Washing'][self.current_task]}")
-
-
         # [LEVEL_1_BLOCK] === INPUT -> frame
         person_outputs, person_infos = null_list, null_list
         if self.config['person']['pipe_enable']:
@@ -176,7 +185,9 @@ class Worker(BaseWorker):
         packet['m_outputs'] = mask_outputs
         packet['m_infos'] = mask_infos
 
+
         # [LEVEL_2_BLOCK] === INPUT -> LEVEL_1_PERSON_BLOCK result
+        # (CROWD)
         crowd_count = null_list
         if self.config['person']['pipe_enable']:
             if self.config['track_opt']['enable']:
@@ -216,30 +227,15 @@ class Worker(BaseWorker):
                         self.config['track_opt']['match_thresh']
                     )['person']
                 for p_output, p_info in zip(person_outputs, person_infos)]
-                crowd_count = [len(person) for person in packet['tracked_person']]
+                crowd_count = [len(person) for person in packet['tracked_person']]   
         packet['crowd_count'] = crowd_count
+        
 
-        mask_wearing_count, no_mask_count = null_list, null_list
-        if self.current_task == 0 and self.config['mask']['pipe_enable']:
-            mask_wearing_count, no_mask_count = [], []
-            for frame, m_output, m_info in zip(packet['frames'], packet['m_outputs'], packet['m_infos']):
-                with_mask, without_mask = [], []
-                if m_output is not None:
-                    mask_out = m_output
-                    # mask_out = mask_outputs[0].cpu()
-                    out = self.MDetector.output_tidy(mask_out, m_info, 0.5)
-                    # self.MDetector.visual_rp(frame, mask_out, m_info, 0.5)
-                    with_mask = out['with_mask']+out['mask_wear_incorrect']
-                    without_mask = out['without_mask']
-                mask_wearing_count.append(len(with_mask))
-                no_mask_count.append(len(without_mask))
-        packet['mask_wearing_count'] = mask_wearing_count
-        packet['no_mask_count'] = no_mask_count
-            
         # [LEVEL_3_BLOCK] === INPUT -> tracked ID and tracked person
         # (HAND)
         no_hand_washing_count, hand_washing_wrong_count, hand_washing_correct_count = null_list, null_list, null_list
-        if self.current_task == 1 and self.config['person']['pipe_enable']:
+        # if self.config['person']['pipe_enable']:
+        if (self.enable_task_scheduling and self.current_task == 0) and self.config['person']['pipe_enable']:
             no_hand_washing_count, hand_washing_wrong_count, hand_washing_correct_count = [], [], []
             for frame, t_person in zip(packet['frames'], packet['tracked_person']):
                 notWashIds, wrongWashIds, correctWashIds = [], [], []
@@ -255,7 +251,8 @@ class Worker(BaseWorker):
 
         # (DISTANCE)
         social_distance, distance_segment_count = null_list, null_list
-        if self.current_task == 2 and self.config['person']['pipe_enable']:
+        # if self.config['person']['pipe_enable']:
+        if (self.enable_task_scheduling and self.current_task == 1) and self.config['person']['pipe_enable']:
             social_distance, distance_segment_count = [], []
             for frame, t_person in zip(packet['frames'], packet['tracked_person']):
                 if self.config['track_opt']['enable']:
@@ -281,7 +278,29 @@ class Worker(BaseWorker):
         packet['social_distance'] = social_distance
         packet['distance_segment_count'] = distance_segment_count
 
-        # [LEVEL_4_BLOCK] === INPUT -> assessment result
+        
+        # [LEVEL_4_BLOCK] === INPUT -> LEVEL_1_PERSON_BLOCK result
+        # (MASK)
+        mask_wearing_count, no_mask_count = null_list, null_list
+        # if self.config['mask']['pipe_enable']:
+        if (self.enable_task_scheduling and self.current_task == 2) and self.config['mask']['pipe_enable']:
+            mask_wearing_count, no_mask_count = [], []
+            for frame, m_output, m_info in zip(packet['frames'], packet['m_outputs'], packet['m_infos']):
+                with_mask, without_mask = [], []
+                if m_output is not None:
+                    mask_out = m_output
+                    # mask_out = mask_outputs[0].cpu()
+                    out = self.MDetector.output_tidy(mask_out, m_info, 0.5)
+                    # self.MDetector.visual_rp(frame, mask_out, m_info, 0.5)
+                    with_mask = out['with_mask']+out['mask_wear_incorrect']
+                    without_mask = out['without_mask']
+                mask_wearing_count.append(len(with_mask))
+                no_mask_count.append(len(without_mask))
+        packet['mask_wearing_count'] = mask_wearing_count
+        packet['no_mask_count'] = no_mask_count
+            
+
+        # [LEVEL_5_BLOCK] === INPUT -> assessment result
         raw_data, short_data = [], []
         for (fid, frame,
             crowdCount,
@@ -294,7 +313,8 @@ class Worker(BaseWorker):
             packet['social_distance'], packet['distance_segment_count'],
             packet['mask_wearing_count'], packet['no_mask_count'],
             packet['no_hand_washing_count'], packet['hand_washing_wrong_count'], packet['hand_washing_correct_count']
-        ):
+        ):  
+            # calculate peroid values
             avg_dist, p_dist = None, None
             mask_ratio, p_mask_ratio = None, None
             p_crowdCount, is_new = None, False
@@ -302,13 +322,22 @@ class Worker(BaseWorker):
             if sdist is not None:
                 avg_dist = sdist / distSegN if distSegN != 0 else 0
                 p_dist, is_new = self.distance_metrics.step(sdist, distSegN)
+                self.overall_metrics["distance"] = p_dist
+
             if maskCount is not None:
                 mask_ratio = maskCount / (maskCount + noMaskCount) if (maskCount + noMaskCount) != 0 else 0.0
                 p_mask_ratio, is_new = self.mask_metrics.step(maskCount, maskCount+noMaskCount)
+                self.overall_metrics["mask"] = p_mask_ratio
+
             if crowdCount is not None:
                 p_crowdCount, is_new = self.person_count_metrics.step(crowdCount, 1)
+                self.overall_metrics["person_count"] = p_crowdCount
+
             if self.config['track_opt']['enable'] and correctWashCount is not None:
                 p_wash, is_new = self.wash_correct_metrics.step(correctWashCount, noWashCount+wrongWashCount+correctWashCount)
+                self.overall_metrics["wash_correct"] = p_wash
+            
+            # show text blocks on screen
             texts = [
                 f'Real-time',
                 f'+ Person Count: {f"{crowdCount:d}" if crowdCount is not None else "not support"}',
@@ -322,12 +351,22 @@ class Worker(BaseWorker):
                 fg_color=(0 ,0 ,0), bg_color=(255, 255, 255, 0.4),
                 point_reverse=(False,True)
             )
+            # For non-scheduling
+            # texts = [
+            #     f'Period-time',
+            #     f'+ Person Count: {f"{p_crowdCount:.2f}" if p_crowdCount is not None else "not support"}',
+            #     f'+ Social Distance: {f"{p_dist / 100:.2f} m" if p_dist is not None else "not support"}',
+            #     f'+ Mask Ratio: {f"{p_mask_ratio * 100:.2f}%" if p_mask_ratio is not None else "not support"}',
+            #     f'+ Correct Wash: {f"{p_wash * 100:.2f} %" if p_wash is not None else "not support"}',
+            # ]
+
+            # For both task-scheduling and non-scheduling
             texts = [
                 f'Period-time',
-                f'+ Person Count: {f"{p_crowdCount:.2f}" if p_crowdCount is not None else "not support"}',
-                f'+ Social Distance: {f"{p_dist / 100:.2f} m" if p_dist is not None else "not support"}',
-                f'+ Mask Ratio: {f"{p_mask_ratio * 100:.2f}%" if p_mask_ratio is not None else "not support"}',
-                f'+ Correct Wash: {f"{p_wash * 100:.2f} %" if p_wash is not None else "not support"}',
+                f'+ Person Count: {self.overall_metrics["person_count"]:.2f}' if self.overall_metrics["person_count"] is not None else '+ Person Count: not support',
+                f'+ Social Distance: {self.overall_metrics["distance"] / 100:.2f} m' if self.overall_metrics["distance"] is not None else '+ Social Distance: not support',
+                f'+ Mask Ratio: {self.overall_metrics["mask"] * 100:.2f}%' if self.overall_metrics["mask"] is not None else '+ Mask Ratio: not support',
+                f'+ Correct Wash: {self.overall_metrics["wash_correct"] * 100:.2f} %' if self.overall_metrics["wash_correct"] is not None else '+ Correct Wash: not support',
             ]
             cmb.VISBlockText(
                 frame,
@@ -336,19 +375,21 @@ class Worker(BaseWorker):
                 fg_color=(255, 255, 255), bg_color=(0 ,0 ,0, 0.4),
                 point_reverse=(True,True)
             )
-            task_names = ['Mask Detection', 'Social Distance', 'Hand Washing']
-            task_texts = [
-                f'Task Status',
-                f'+ Current: {task_names[self.current_task]}',
-                f'+ Time Left: {(frames_per_switch - self.task_timer) / self.fps:.1f}s'
-            ]
-            cmb.VISBlockText(
-                frame,
-                task_texts, (20, 20),  
-                ratio=1, thickness=3, 
-                fg_color=(0, 0, 0), bg_color=(255, 255, 255, 0.4),
-                point_reverse=(False,False)
-            )
+            # 如果進行任務分配，顯示當前任務
+            if self.enable_task_scheduling:
+                task_names = ['Hand Washing', 'Social Distance',  'Mask Detection']
+                task_texts = [
+                    f'Task Status',
+                    f'+ Current: {task_names[self.current_task]}',
+                    f'+ Time Left: {(frames_per_task - self.task_timer) / self.fps:.1f}s'
+                ]
+                cmb.VISBlockText(
+                    frame,
+                    task_texts, (20, 20),  
+                    ratio=1, thickness=3, 
+                    fg_color=(0, 0, 0), bg_color=(255, 255, 255, 0.4),
+                    point_reverse=(False,False)
+                )
 
 
             hours, remain_second_frame = math.floor(fid / (3600*self.fps)), fid % (3600*self.fps)
@@ -372,10 +413,24 @@ class Worker(BaseWorker):
                     'mask_ratio':p_mask_ratio,
                     'correct_wash':p_wash,
                 })
+                # short_data.append({
+                #     'end_frame_id':fid,
+                #     'end_play_time':f"{hours:02d}:{minutes:02d}:{seconds:02d}.{ms:03d}",
+                #     'person_count':self.overall_metrics["person_count"],
+                #     'social_distance':self.overall_metrics["distance"],
+                #     'mask_ratio':self.overall_metrics["mask"],
+                #     'correct_wash':self.overall_metrics["wash_correct"],
+                # })
         packet['raw_data'] = raw_data
         packet['short_data'] = short_data
 
-        # [LEVEL_5_BLOCK] === OUTPUT
+        # 如果進行任務分配且達指定的處理禎數,切換到下一個任務
+        if self.enable_task_scheduling and (self.task_timer >= frames_per_task):
+            self.current_task = (self.current_task + 1) % 3
+            self.task_timer = 0
+            logger.info(f"Switching to task: {['Hand Washing', 'Social Distance', 'Mask Detection'][self.current_task]}")
+
+        # [LEVEL_6_BLOCK] === OUTPUT
         return packet['fids'], packet['frames'], packet['raw_data'], packet['short_data']
 
     def _conditionWork(self):
